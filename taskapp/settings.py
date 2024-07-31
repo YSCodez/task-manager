b06bbeedc07cde578d512a74b3d5cac54377d3a4
Yash Sakhare <96004797+YSCodez@users.noreply.github.com>
2024-07-31 15:41:50 +0530

    Deployment changes

--- taskapp/settings.py
+++ taskapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-me2)2ror!!xgrlf+j3l=**!vkybe^a6c6$h+1oy%2gz5ximo9m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
